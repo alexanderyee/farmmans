eee9daf460aaf61be18e522d6237b84892ed0a07
Alex Yee <yeealexi@gmail.com>
2025-02-10 23:05:58 -0800

    add cow animations

---
+++ scripts/animals/cow.gd
@@ -0,0 +1,13 @@
+class_name Cow
+extends CharacterBody2D
+
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+
+var should_randomly_move := false
+
+
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	pass
